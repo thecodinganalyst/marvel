8dc22b0b657e7a845fd86713f2922fd29833e130
Dennis Cai <iamdenniscai@gmail.com>
2021-10-19 23:00:15 +0800

    change default port to 8080

--- src/app.ts
+++ src/app.ts
@@ -19,7 +19,7 @@ class App {
 
   constructor(routes: Routes[]) {
     this.app = express();
-    this.port = process.env.PORT || 3000;
+    this.port = process.env.PORT || 8080;
     this.env = process.env.NODE_ENV || 'development';
 
     this.initializeMiddlewares();
